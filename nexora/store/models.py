eecbeafd7777732b0615e5da69e53af488e4bbbf
Miraculous-bkm <miraclebenjamin050@gmail.com>
2024-10-07 13:43:06 -0400

    Restored deleted files

--- nexora/store/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class NewsletterSubscriber(models.Model):
-    email = models.EmailField(unique=True)
-    channels = models.CharField(max_length=255)
-    subscribed_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.email
--- nexora/store/static/js/slided.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.slide-bland').forEach(slideshow => {
-    slideshow.addEventListener('mouseover', () => {
-        slideshow.style.animationPlayState = 'paused';
-    });
-    slideshow.addEventListener('mouseout', () => {
-        slideshow.style.animationPlayState = 'running';
-    });
-});
-
